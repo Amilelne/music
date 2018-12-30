7b3fbaf3b44fe4698bd2f20b9b98a5f042ac50ac
Amelie <Amil3lee@gmail.com>
2018-12-30 20:35:26 +0800

    Implement login and register functions

--- server/resolvers/user.resolver.js
+++ server/resolvers/user.resolver.js
@@ -1,14 +1,29 @@
-const { User } = require('../models');
+const { User, UserAuth } = require('../models');
 
 const resolverMap = {
   Query: {
     users: async (obj, args, context, info) => {
       return User.find();
+    },
+    login: async (obj, { identifier, credential }, context, info) => {
+      return UserAuth.findOne({ identifier, credential }).then((user) => {
+        return User.findById(user.userId);
+      });
     }
   },
   Mutation: {
     addUser: async (obj, { data }, context, info) => {
       return User.create(data);
+    },
+    register: async (obj, { data }, context, info) => {
+      return User.create({ name: data.name }).then((user) => {
+        UserAuth.create({
+          userId: user._id,
+          identifier: data.identifier,
+          credential: data.credential
+        });
+        return user;
+      });
     }
   }
 };
