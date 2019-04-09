1da9427eb2e03a39d64d74a9a2de40a06e9184df
Amilelne <amelie@Amelies-MacBook-Pro.local>
2019-04-09 08:35:28 +0800

    Deploy by express

--- server/app.js
+++ server/app.js
@@ -1,13 +1,21 @@
-const { configure } = require('./config');
-const express = require('express');
-const cors = require('cors');
+const { configure } = require("./config");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const mongoose = require('./mongoose');
+const mongoose = require("./mongoose");
 
+const distDir = "/dist/music-ai";
 const app = express();
 app.use(cors());
+// serve front-end
+app.use(express.static(path.resolve(process.cwd() + distDir)));
 // upload files
-app.use(express.static(process.cwd() + '/server/uploads'));
+app.use(express.static(path.resolve(process.cwd() + "/server/uploads")));
+// redirect router
+app.use(/^((?!(graphql)).)*/, (req, res) => {
+  res.sendFile(path.join(process.cwd(), distDir + "/index.html"));
+});
 // Get configures form config file
 const { server } = configure;
 for (const key of Object.keys(server)) {
@@ -15,9 +23,6 @@ for (const key of Object.keys(server)) {
 }
 
 // Connect to DB
-mongoose.connect(
-  mongoose.get('db_uri'),
-  { useNewUrlParser: true }
-);
+mongoose.connect(mongoose.get("db_uri"), { useNewUrlParser: true });
 
 module.exports = app;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
-const app = require('./app');
-const { ApolloServer } = require('apollo-server-express');
-const { resolvers } = require('./resolvers');
-const extractors = require('./utils/extractors.service');
-const JWT = require('./utils/jwt.service');
-const readGqlFiles = require('./utils/readGqlFiles');
+const app = require("./app");
+const { ApolloServer } = require("apollo-server-express");
+const { resolvers } = require("./resolvers");
+const extractors = require("./utils/extractors.service");
+const JWT = require("./utils/jwt.service");
+const readGqlFiles = require("./utils/readGqlFiles");
 
 (async () => {
   const typeDefs = await readGqlFiles();
@@ -12,7 +12,7 @@ const readGqlFiles = require('./utils/readGqlFiles');
     resolvers,
     context: ({ req }) => {
       // header_name is defined in graphql.module.ts
-      const header_name = 'authorization';
+      const header_name = "authorization";
       // try to retrieve a user with the token
       let token = extractors.fromHeaderAsBearerToken(header_name)(req);
       let user = null;
@@ -24,10 +24,10 @@ const readGqlFiles = require('./utils/readGqlFiles');
     }
   });
   server.applyMiddleware({ app });
+
+  const port = app.get("port");
+
+  app.listen({ port }, () => {
+    console.log(`Server ready at ${port}`);
+  });
 })();
-
-const port = app.get('port');
-
-app.listen({ port }, () => {
-  console.log(`Server ready at ${port}`);
-});
