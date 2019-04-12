1c655ede0af177006ca8acc274b2086dd36e1832
Amilelne <amelie@Amelies-MacBook-Pro.local>
2019-04-12 08:58:40 +0800

    Fix bugs in expert-detail when building in prod

--- src/app/expert/expert-detail/expert-detail.component.ts
+++ src/app/expert/expert-detail/expert-detail.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 import { ExpertService } from "../expert.service";
 import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-expert-detail',
-  templateUrl: './expert-detail.component.html',
-  styleUrls: ['./expert-detail.component.scss']
+  selector: "app-expert-detail",
+  templateUrl: "./expert-detail.component.html",
+  styleUrls: ["./expert-detail.component.scss"]
 })
 export class ExpertDetailComponent implements OnInit {
   @Input() expert: any;
-  constructor(private expertService: ExpertService,
-    private route: ActivatedRoute) { }
+  constructor(
+    private expertService: ExpertService,
+    private route: ActivatedRoute
+  ) {}
 
   private expertId;
+  color;
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.expertId = params.id;
@@ -20,7 +23,6 @@ export class ExpertDetailComponent implements OnInit {
 
     this.expertService.getExpertDetail(this.expertId).subscribe(data => {
       this.expert = data;
-    })
+    });
   }
-
 }
